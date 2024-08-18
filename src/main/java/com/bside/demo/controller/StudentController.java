779ae8f828888335e160fd772e4133fc61d1bb5d
Daniel Tiro Bravo <daniel@MacBook-Pro-de-Daniel.local>
2024-08-17 21:59:27 -0600

    adding curp rest service

--- src/main/java/com/bside/demo/controller/StudentController.java
+++ src/main/java/com/bside/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.bside.demo.controller;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
 
@@ -10,7 +11,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,9 +28,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.bside.demo.entity.Student;
 import com.bside.demo.exception.StudentAlreadyExistsException;
+import com.bside.demo.exception.StudentArgumentNotValidException;
 import com.bside.demo.exception.StudentNotFoundException;
 import com.bside.demo.service.IStudentService;
 
@@ -87,6 +94,27 @@ public class StudentController {
 			student.setActive(false);
 			studentService.save(student);
 		}
+	}
+	
+	@GetMapping("/valida/curp/{curp}")
+	public ResponseEntity<String> validaCurp(@PathVariable(name = "curp", required = true) String curp){
+		if(curp.length() != 18) {
+			throw new StudentArgumentNotValidException("Longitude not valid for curp: " + curp);
+		}
+		String url = "https://apimarket.mx/api/renapo/grupo/valida-curp?curp=" + curp;
+		String token = "760825e9-73eb-4d0c-b1d5-83a47d6de581";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		headers.setBearerAuth(token);
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+		if (response.getStatusCode() == HttpStatus.OK) {
+			return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+		}else {
+			throw new StudentArgumentNotValidException(response.getStatusCode().toString());
+		}
+			
 	}
 
 }
